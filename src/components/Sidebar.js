73fb83fce863be1d04f629546d9e0dbd14d9dc64
Angela Canning <46754565+AngelaMaC@users.noreply.github.com>
2022-09-23 15:11:35 -0500

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,49 @@
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='menu'>
+                <div className='profile'>
+                    <img className='profile-image' alt='user' src='./images/profile_image.jpeg'></img>
+                    <p className='welcome'>Welcome, Dr. Smith</p>
+                    <div className='date-today'>March 21, 2022</div>
+                </div>
+                <hr className='border' />
+                <ul id='navigation'>
+                    <li className='menu-items'>
+                        <div className='nav' data-route='Appointments'>
+                            Appointments
+                        </div>
+                    </li>
+                    <li className='menu-items'>
+                        <div className='nav' data-route='Patients'>
+                            Patients
+                        </div>
+                    </li>
+                    <li className='menu-items'>
+                        <div className='nav' data-route='PatientRecords'>
+                            Patient Records
+                        </div>
+                    </li>
+                    <li className='menu-items'>
+                        <div className='nav' data-route='Settings'>
+                            Settings
+                        </div>
+                    </li>
+                    <li className='menu-items'>
+                        <div className='nav' data-route='Admin'>
+                            Admin
+                        </div>
+                    </li>
+                    <li className='menu-items'>
+                        <div className='nav' data-route='Logout'>
+                            Logout
+                        </div>
+                    </li>
+                </ul>
+                <hr className='border' />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
